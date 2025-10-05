326a5562aef3edabec784163c804ae0d2b8b90ce
Angel-G-Soto <angel.soto12@upr.edu>
2025-10-05 11:30:41 -0400

    Venue and Use Requirement Models
    Signed-off-by: Angel-G-Soto <angel.soto12@upr.edu>

--- app/Models/UseRequirements.php
+++ app/Models/UseRequirements.php
@@ -3,8 +3,40 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class UseRequirements extends Model
 {
-    //
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'venue_id';
+
+    /**
+     * The database connection that should be used by the model.
+     *
+     * @var string
+     */
+    protected $connection = 'mariadb';
+
+    /**
+     * The attributes that are mass assignable.
+     * @var string[]
+     */
+    protected $fillable = [
+        'us_doc_drive',
+        'us_instructions',
+    ];
+
+    /**
+     * Relationship between the Use Requirement and Venue
+     * @return BelongsTo
+     */
+    public function venue(): BelongsTo
+    {
+        return $this->belongsTo(Venue::class, 'venue_id');
+    }
 }
--- app/Models/Venue.php
+++ app/Models/Venue.php
@@ -3,8 +3,62 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Venue extends Model
 {
-    //
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'venue_id';
+
+    /**
+     * The database connection that should be used by the model.
+     *
+     * @var string
+     */
+    protected $connection = 'mariadb';
+
+    /**
+     * The attributes that are mass assignable.
+     * @var string[]
+     */
+    protected $fillable = [
+        'v_name',
+        'v_code',
+        'v_department',
+        'v_features',
+        'v_capacity',
+        'v_test_capacity',
+    ];
+
+    /**
+     * Relationship between the Venue and User
+     * @return BelongsTo
+     */
+//    public function user(): BelongsTo
+//    {
+//        return $this->belongsTo(User::class, 'user_id');
+//    }
+
+    /**
+     * Relationship between the Venue and Use Requirement
+     * @return HasMany
+     */
+    public function requirements(): HasMany
+    {
+        return $this->hasMany(UseRequirements::class, 'use_requirement_id');
+    }
+
+    /**
+     * Relationship between the Venue and Event
+     * @return HasMany
+     */
+    public function requests(): HasMany
+    {
+        return $this->hasMany(Event::class, 'event_id');
+    }
 }
