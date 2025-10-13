d4f3d0a3f08f4459c2e544ed0520b79a084cd519
Angel-G-Soto <angel.soto12@upr.edu>
2025-10-13 01:32:15 -0400

    Finished tests. Add role testing once relevant models are added.
    Signed-off-by: Angel-G-Soto <angel.soto12@upr.edu>

--- app/Services/VenueService.php
+++ app/Services/VenueService.php
@@ -10,6 +10,7 @@ use Exception;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use App\Services\DepartmentService;
+use InvalidArgumentException;
 
 class VenueService {
     /**
