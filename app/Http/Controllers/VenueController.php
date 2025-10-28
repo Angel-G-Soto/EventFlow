14f863fd69db6b776c846e168fe27f66287bfa43
Angel-G-Soto <angel.soto12@upr.edu>
2025-10-28 12:58:34 -0400

    Patched VenueController.php with app()
    Signed-off-by: Angel-G-Soto <angel.soto12@upr.edu>

--- app/Http/Controllers/VenueController.php
+++ app/Http/Controllers/VenueController.php
@@ -13,107 +13,6 @@ use Illuminate\Support\Facades\Auth;
 class VenueController extends Controller
 {
 
-//    /**
-//     * Display a listing of the resource.
-//     */
-//    public function index()
-//    {
-//        // Policy
-//
-//        //$venues = new VenueService()->getAllVenues(['department_id' => Auth::user()->department_id]);
-//
-//        return view('venues.index' /*['venues' => $venues]*/);
-//    }
-//
-//    /**
-//     * Show the form for creating a new resource.
-//     */
-//    public function create()
-//    {
-//        //
-//    }
-//
-//    /**
-//     * Store a newly created resource in storage.
-//     */
-//    public function store(Request $request)
-//    {
-//        //
-//    }
-//
-//    /**
-//     * Display the specified resource.
-//     */
-//    public function show(int $venue_id)
-//    {
-//        // Policy
-//
-//        $venue = VenueService::getVenueById($venue_id);
-//
-//        return view('venues.show', ['venue' => $venue]);
-//    }
-//
-//    /**
-//     * Show the form for editing the specified resource.
-//     */
-//    public function edit(int $venue_id)
-//    {
-//        // Policy
-//
-//        $venue = VenueService::getVenueById($venue_id);
-//
-//        return view('venues.edit', ['venue' => $venue]);
-//    }
-//
-//    /**
-//     * Update the specified resource in storage.
-//     */
-//    public function update(Request $request, int $venue_id)
-//    {
-//        //
-//    }
-//
-//    /**
-//     * Remove the specified resource from storage.
-//     */
-//    public function destroy(int $venue_id)
-//    {
-//        //
-//    }
-//
-//    public function show_requirements(int $venue_id)
-//    {
-//        // Policy
-//
-//        $venue = VenueService::getVenueById($venue_id);
-//
-//        $requirements = VenueService::getUseRequirements($venue_id);
-//
-//        return view('venues.showRequirements', ['venue' => $venue, 'requirements' => $requirements]);
-//    }
-//
-//    public function edit_requirements(int $venue_id)
-//    {
-//        // Policy
-//
-//        $venue = VenueService::getVenueById($venue_id);
-//
-//        $requirements = VenueService::getUseRequirements($venue_id);
-//
-//        return view('venues.editRequirements', ['venue' => $venue, 'requirements' => $requirements]);
-//    }
-//
-//    public function update_requirements(int $venue_id)
-//    {
-//
-//    }
-
-
-
-
-
-    /////////////////////////////////////////////////// REVAMP /////////////////////////////////////////////////////////
-
     ////////// GET
 
     public function managerVenueIndex()
@@ -122,7 +21,8 @@ class VenueController extends Controller
         if (!Auth::user()->getRoleNames()->contains('venue-manager')) abort(403, 'Only managers can access this page.');
 
         // Run service that brings the respective venues
-        $venues = VenueService::getAllVenues(['department_id' => Auth::user()->department()->id]);
+        $venueService = app(VenueService::class);
+        $venues = $venueService->getAllVenues(['department_id' => Auth::user()->department()->id]);
 
         // Load view
         return view('venue.managerVenueIndex', ['venues' => $venues]);
@@ -134,7 +34,8 @@ class VenueController extends Controller
         if (!Auth::user()->getRoleNames()->contains('department-director')) abort(403, 'Only directors can access this page.');
 
         // Run service that brings the respective venues
-        $venues = VenueService::getAllVenues(['department_id' => Auth::user()->department()->id]);
+        $venueService = app(VenueService::class);
+        $venues = $venueService->getAllVenues(['department_id' => Auth::user()->department()->id]);
 
         // Load view
         return view('venue.directorVenueIndex', ['venues' => $venues]);
@@ -146,7 +47,8 @@ class VenueController extends Controller
         if (!Auth::user()->getRoleNames()->contains('system-administrator')) abort(403, 'Only administrators can access this page.');
 
         // Run service that brings all the venues
-        $venues = VenueService::getAllVenues();
+        $venueService = app(VenueService::class);
+        $venues = $venueService->getAllVenues();
         // Load view
         return view('venue.administratorVenueIndex', ['venues' => $venues]);
     }
@@ -171,7 +73,8 @@ class VenueController extends Controller
         ]);
 
         // Call service that updates requests
-        VenueService::updateOrCreateVenueRequirements(VenueService::findById($venue_id),$validated['requirements'],Auth::user());
+        $venueService = app(VenueService::class);
+        $venueService->updateOrCreateVenueRequirements($venueService->findById($venue_id),$validated['requirements'],Auth::user());
 
         // Reload page
         redirect('');
@@ -181,10 +84,11 @@ class VenueController extends Controller
     {
         // Abort if the venue id is not valid
         if ($venue_id < 0) {abort(400, 'Venue id must be greater than zero.');}
-        if (!VenueService::findById($venue_id)) {abort(404, 'Venue not found.');}
+        $venueService = app(VenueService::class);
+        if (!$venueService->findById($venue_id)) {abort(404, 'Venue not found.');}
 
         // Run policy that verifies the user is manager and part of the department
-        $this->authorize('updateAvailability', [Auth::user(), !VenueService::findById($venue_id)]);
+        $this->authorize('updateAvailability', [Auth::user(), !$venueService->findById($venue_id)]);
 
         // Validate request
         $validated = $request->validate([
@@ -193,7 +97,7 @@ class VenueController extends Controller
         ]);
 
         // Call service that updates the working hours of the venue
-        VenueService::updateVenueOperatingHours(VenueService::findById($venue_id),$validated['opening_hours'],$validated['closing_hours'],Auth::user());
+        $venueService->updateVenueOperatingHours($venueService->findById($venue_id),$validated['opening_hours'],$validated['closing_hours'],Auth::user());
 
         // Reload page
         redirect('');
@@ -203,10 +107,11 @@ class VenueController extends Controller
     {
         // Abort if the venue id is not valid
         if ($venue_id < 0) {abort(400, 'Venue id must be greater than zero.');}
-        if (!VenueService::findById($venue_id)) {abort(404, 'Venue not found.');}
+        $venueService = app(VenueService::class);
+        if (!$venueService->findById($venue_id)) {abort(404, 'Venue not found.');}
 
         // Run policy that verifies the user is director and part of the department
-        $this->authorize('assignManager', [Auth::user(), !VenueService::findById($venue_id)]);
+        $this->authorize('assignManager', [Auth::user(), !$venueService->findById($venue_id)]);
 
         // Validate request
         $validated = $request->validate([
@@ -214,7 +119,8 @@ class VenueController extends Controller
         ]);
 
         // Call service that updates the manager
-        VenueService::assignManager(VenueService::findById($venue_id),UserService::findUserById($validated['user_id']),Auth::user());
+        $userService = app(UserService::class);
+        $venueService->assignManager($venueService->findById($venue_id),$userService->findUserById($validated['user_id']),Auth::user());
 
         // Reload page
         redirect('');
@@ -224,38 +130,18 @@ class VenueController extends Controller
     {
         // Abort if the venue id is not valid
         if ($venue_id < 0) {abort(400, 'Venue id must be greater than zero.');}
-        if (!VenueService::findById($venue_id)) {abort(404, 'Venue not found.');}
+        $venueService = app(VenueService::class);
+        if (!$venueService->findById($venue_id)) {abort(404, 'Venue not found.');}
 
         // Run policy that verifies that the user is the admin
         $this->authorize('deleteVenue', Auth::user());
 
         // Call service that deletes the model
-        VenueService::deactivateVenues([VenueService::findById($venue_id)],Auth::user());
+        $venueService->deactivateVenues([$venueService->findById($venue_id)],Auth::user());
 
         // Reload page
         redirect('');
     }
-
-//    public function deleteSelectedVenues(Request $request)
-//    {
-//        // Run policy that verifies that the user is the admin
-//        $this->authorize('deleteVenue', Auth::user());
-//
-//        // Validate request
-//        $validated = request()->validate([
-//            'venue_ids' => 'required|array|min:1|max:10',
-//            'venue_ids.*' => 'integer|distinct|exists:venues,id',
-//        ]);
-//
-//        // Call services that acquires models
-//        $venues = VenueService::getVenuesByIds($validated['venue_ids']);
-//
-//        // Call service that deletes the models
-//        VenueService::deactivateVenues($venues,Auth::user());
-//
-//        // Reload page
-//        redirect('');
-//    }
 
     public function storeVenue(Request $request)
     {
@@ -277,7 +163,8 @@ class VenueController extends Controller
         ]);
 
         // Call service that creates the venue
-        VenueService::createVenue($validated,Auth::user());
+        $venueService = app(VenueService::class);
+        $venueService->createVenue($validated,Auth::user());
 
         // Reload page
         redirect('');
@@ -302,7 +189,8 @@ class VenueController extends Controller
         ]);
 
         // Call service that creates the venue
-        $venue = VenueService::updateVenue(VenueService::getVenuesByIds($venue_id), $validated, Auth::user());
+        $venueService = app(VenueService::class);
+        $venue = $venueService->updateVenue($venueService->getVenuesByIds($venue_id), $validated, Auth::user());
 
         // Reload page
         redirect('');
