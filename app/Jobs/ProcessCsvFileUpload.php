ea72cbeab27b25f143c875140ab961c60e76eb78
andrestorres18 <andres.torres18@upr.edu>
2025-12-02 23:51:15 -0400

    Changed email parameters

--- app/Jobs/ProcessCsvFileUpload.php
+++ app/Jobs/ProcessCsvFileUpload.php
@@ -109,7 +109,7 @@ class ProcessCsvFileUpload implements ShouldQueue
         $scanUnavailable = false;
 
 //        try {
-            $scan = new Process([config('services.clamav.scan_path'), '--fdpass', $filePath]);
+            $scan = new Process([trim((string) config('services.clamav.scan_path')), '--fdpass', $filePath]);
             $scan->run();
             $output = $scan->getOutput() . "\n" . $scan->getErrorOutput();
             // dd($output);
